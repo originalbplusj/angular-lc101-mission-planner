e097189a9f435e5a3cc2c037a6cd982a0c40cf23
originalbplusj <originalbplusj@yahoo.com>
2022-03-08 11:18:11 -0600

    equipment bonus

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
+  equipment: string[] = ['Habitat dome', 'Drones', 'Food containers', 'Oxygen tanks'];
+
+  itemBeingEdited: string = null;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(equipmentName: string) {
+    this.equipment.push(equipmentName);
+  }
+
+  remove(equipmentName: string) {
+    let index = this.equipment.indexOf(equipmentName);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(equipmentName: string) {
+    this.itemBeingEdited = equipmentName;
+  }
+
+  save(equipmentName: string) {
+    equipmentName = equipmentName;
+    this.itemBeingEdited = null;
+  }
 }
