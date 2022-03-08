33a09082fb6dc00db3d6b93df69284327ba1a741
originalbplusj <originalbplusj@yahoo.com>
2022-03-08 11:46:33 -0600

    experiments commit

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: string[] = ['Mars soil sample', 'Plant growth in habitat', 'Human bone density'];
+
+  itemBeingEdited: string = null;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(experimentName: string) {
+    this.experiments.push(experimentName);
+  }
+
+  remove(experimentName: string) {
+    let index = this.experiments.indexOf(experimentName);
+    this.experiments.splice(index, 1);
+  }
+
+  edit(experimentName: string) {
+    this.itemBeingEdited = experimentName;
+  }
+  save(experimentName: string) {
+    experimentName = experimentName;
+    this.itemBeingEdited = null;
+  }
 }
